6919601bf65c224bbedf1519298b6d8f1265a037
MoOmEeN <moomeen@gmail.com>
2016-03-19 00:36:44 +0100

    contact form

--- app/com/keendly/controllers/api/error/Error.java
+++ app/com/keendly/controllers/api/error/Error.java
@@ -4,7 +4,7 @@ public enum Error {
 
     DELIVERY_EMAIL_NOT_CONFIGURED("Send-to-Kindle email address not configured"),
     TOO_MANY_ITEMS("Max number of feeds in single delivery: %d"),
-    WRONG_EMAIL("Email address incorrect, allowed are only: %s");
+    WRONG_EMAIL("Email address incorrect, allowed domains  only: %s");
 
     private String message;
 
