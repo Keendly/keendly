6f19ee14e65ea646d93d50873e14f5232b1a99a5
MoOmEeN <moomeen@gmail.com>
2016-05-18 23:42:19 +0200

    fix syntax error when there is no subscriptiosn to fulfill

--- app/com/keendly/dao/SubscriptionDao.java
+++ app/com/keendly/dao/SubscriptionDao.java
@@ -8,6 +8,7 @@ import play.db.jpa.JPA;
 import javax.persistence.Query;
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.TimeZone;
 
@@ -32,21 +33,25 @@ public class SubscriptionDao {
         // TODO should probably take into account ones that DO have deliveries but were not actually delivered
         Query query = JPA.em()
                 .createNativeQuery("select s.id from subscription s " +
-                        "where s.frequency = 'DAILY' and not exists (" +
+                        "where s.active = TRUE and s.frequency = 'DAILY' and not exists (" +
                         "   select id from delivery d where d.subscription_id = s.id " +
                         "       and d.created at time zone :system_timezone > case " +
                         "               when cast(now() at time zone s.timezone as time) > cast(s.time as time) " + // if today the scheduled hour has passed
                         "               then to_timestamp(to_char(now() at time zone s.timezone,'YYYY-MM-DD ')||s.time, 'YYYY-MM-DD HH24:MI') " + // then last scheduled delivery was today
-                        "               else to_timestamp(to_char((now() at time zone s.timezone)- interval '1 day' ,'YYYY-MM-DD ')||s.time, 'YYYY-MM-DD HH24:MI') " + // otherwise yesterday
+                        "               else to_timestamp(to_char((now() at time zone s.timezone) - interval '1 day', 'YYYY-MM-DD ')||s.time, 'YYYY-MM-DD HH24:MI') " + // otherwise yesterday
                         "       end)").setParameter("system_timezone", TimeZone.getDefault().getID());
-
         List r = query.getResultList();
         List<Long> ids = new ArrayList<>();
         for (Object o : r) {
             ids.add(((BigInteger) o).longValue());
         }
-        List res = JPA.em().createQuery("select s from SubscriptionEntity s where s.id in (:ids)")
-                .setParameter("ids", ids).getResultList();
-        return res;
+        if (!ids.isEmpty()) {
+            List res = JPA.em().createQuery("select s from SubscriptionEntity s where s.id in (:ids)")
+                    .setParameter("ids", ids).getResultList();
+            return res;
+        } else {
+            return Collections.emptyList();
+        }
+
     }
 }
